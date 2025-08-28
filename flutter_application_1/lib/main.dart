67994590beb616135ee4be545850ccffef02fef0
AkumaShachi <koonguide4480@gmail.com>
2025-08-28 14:38:11 +0700

    bra bra

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,10 +1,5 @@
 import 'package:flutter/material.dart';
-import 'forgetscreen.dart';
 import 'loginscreen.dart';
-import 'nurses_screen/nurse_add_case.dart';
-import 'nurses_screen/nurse_ex-post_case.dart';
-import 'nurses_screen/nurse_list_case.dart';
-import 'resetpassword.dart';
 
 void main() {
   runApp(const MyApp());
